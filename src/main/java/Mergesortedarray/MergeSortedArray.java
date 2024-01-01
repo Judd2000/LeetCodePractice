1873e855ed4c253b13375671520683271d12ef77
Judd2000 <njudd.mail@gmail.com>
2024-01-01 10:24:08 -0800

    LeetCode test commit

--- src/main/java/Mergesortedarray/MergeSortedArray.java
+++ src/main/java/Mergesortedarray/MergeSortedArray.java
@@ -18,6 +18,7 @@ public class MergeSortedArray {
         if (nums1.length != m + n || nums1.length != m + nums2.length) {
             throw new InvalidParameterException("First array must have space for elements of the second array!");
         }
+
         int i = m-1 ,j = n-1;
         int k = m + n -1;
         // take advantage of their sorted order and compare last elements - gradually move them to the end of the list.
@@ -27,7 +28,7 @@ public class MergeSortedArray {
             else
                 nums1[k--] = nums2[j--];
         }
-        // edge case where the nums1 array is empty - fill with nums2
+        // edge case where the nums1 array is empty - fill with nums2.
         while (j >= 0) {
             nums1[k--] = nums2[j--];
         }
