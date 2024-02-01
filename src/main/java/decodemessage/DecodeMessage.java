285a97a44ee085afbd4cab3d669769ca8e94f4c2
Judd2000 <njudd.mail@gmail.com>
2024-02-01 15:11:54 -0700

    DecodedMessage solution

--- src/main/java/decodemessage/DecodeMessage.java
+++ src/main/java/decodemessage/DecodeMessage.java
@@ -7,17 +7,25 @@ public class DecodeMessage {
     public String decodeMessage(String key, String message) {
         Map<Character, Character> cipher = new HashMap<>();
         final int[] counter = {0};
+        // construct the cipher
         for (int i = 0; i < key.length(); i++) {
             char curChar = key.charAt(i);
-            if (curChar == ' ') {
-                continue;
+            if (Character.isAlphabetic(curChar)) {
+                char charToMap = (char) ('a' + counter[0]);
+                cipher.computeIfAbsent(curChar, k -> {
+                    counter[0]++;
+                    return charToMap;
+                });
             }
-            char charToMap = (char) ('a' + counter[0]);
-            cipher.computeIfAbsent(curChar, k -> {
-                counter[0]++;
-                return charToMap;
-            });
         }
-        return "";
+        // now, substitute.
+        char[] messageArray = message.toCharArray();
+        for (int i = 0; i < messageArray.length; i++) {
+            char curChar = messageArray[i];
+            if (curChar != ' ') {
+                messageArray[i] = cipher.get(curChar);
+            }
+        }
+        return new String(messageArray);
     }
 }
--- src/test/java/decodemessage/DecodeMessageTest.java
+++ src/test/java/decodemessage/DecodeMessageTest.java
@@ -3,6 +3,8 @@ package decodemessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DecodeMessageTest {
     DecodeMessage message;
 
@@ -15,7 +17,18 @@ public class DecodeMessageTest {
     void testDecodeMessage() {
         // Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
         // Output: "this is a secret"
-        String key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv";
-        this.message.decodeMessage(key, message);
+        String key = "the quick brown fox jumps over the lazy dog";
+        String message = "vkbs bs t suepuv";
+        String expected = "this is a secret";
+
+        assertEquals(expected, this.message.decodeMessage(key, message));
+
+        // Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+        // Output: "the five boxing wizards jump quickly"
+        key = "eljuxhpwnyrdgtqkviszcfmabo";
+        message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb";
+        expected = "the five boxing wizards jump quickly";
+
+        assertEquals(expected, this.message.decodeMessage(key, message));
     }
 }
